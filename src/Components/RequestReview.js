1291b70ecf28ca63a949b6f8dca906cc558caece
PMafra <pmafrarm@gmail.com>
2021-09-03 14:03:03 -0300

    Minor line-height change in review.css; home-button added and working in RequestReview component

--- src/Components/RequestReview.js
+++ src/Components/RequestReview.js
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 export default function RequestReview ({ movieAndDateAndUserInfo }) {
 
     console.log(movieAndDateAndUserInfo);
@@ -9,39 +11,44 @@ export default function RequestReview ({ movieAndDateAndUserInfo }) {
                     Pedido feito com sucesso!
                 </h1>
             </div>
-            <div className="choice-review-container">
-                <div className="request-info-box">
-                    <h2>
-                        Filme e sessão
-                    </h2>
-                    <span>
-                        {movieAndDateAndUserInfo.movieAndSession.title}
-                    </span>
-                    <span>
-                        {`${movieAndDateAndUserInfo.movieAndSession.date} ${movieAndDateAndUserInfo.movieAndSession.time}`}
-                    </span>
-                </div>
-                <div className="request-info-box">
-                    <h2>
-                        Ingressos
-                    </h2>
-                    {movieAndDateAndUserInfo.tickets[0].map(ticket => 
+            <div className="review-page">
+                <div className="choice-review-container">
+                    <div className="request-info-box">
+                        <h2>
+                            Filme e sessão
+                        </h2>
                         <span>
-                            Assento {ticket.name}
+                            {movieAndDateAndUserInfo.movieAndSession.title}
                         </span>
-                    )}
+                        <span>
+                            {`${movieAndDateAndUserInfo.movieAndSession.date} ${movieAndDateAndUserInfo.movieAndSession.time}`}
+                        </span>
+                    </div>
+                    <div className="request-info-box">
+                        <h2>
+                            Ingressos
+                        </h2>
+                        {movieAndDateAndUserInfo.tickets[0].map(ticket => 
+                            <span>
+                                Assento {ticket.name}
+                            </span>
+                        )}
+                    </div>
+                    <div className="request-info-box">
+                        <h2>
+                            Comprador
+                        </h2>
+                        <span>
+                            Nome: {movieAndDateAndUserInfo.buyer.buyerName}
+                        </span>
+                        <span>
+                            CPF: {movieAndDateAndUserInfo.buyer.buyerCPF}
+                        </span>
+                    </div>
                 </div>
-                <div className="request-info-box">
-                    <h2>
-                        Comprador
-                    </h2>
-                    <span>
-                        Nome: {movieAndDateAndUserInfo.buyer.buyerName}
-                    </span>
-                    <span>
-                        CPF: {movieAndDateAndUserInfo.buyer.buyerCPF}
-                    </span>
-                </div>
+                <Link className="home-button" to="/">
+                    Voltar para Home
+                </Link>
             </div>
         </div>
     )
